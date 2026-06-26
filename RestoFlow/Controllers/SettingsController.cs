20ed4b5a6a1105ef0ce98398571043bc40e45c11
abedelrahmanOpenTech <68178460+AbedelrahmanD@users.noreply.github.com>
2026-06-26 18:33:14 +0300

    Update SettingsController.cs

--- RestoFlow/Controllers/SettingsController.cs
+++ RestoFlow/Controllers/SettingsController.cs
@@ -10,7 +10,7 @@ namespace RestoFlow.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-  // [Authorize(Roles = "Admin")]
+   [Authorize(Roles = "Admin")]
     public class SettingsController : ControllerBase
     {
         private readonly ISettingService _service;
